2f5787a2fdf319007ba40d23f67b91aed68bad4d
TomiFG <tomi.fernandezg@gmail.com>
2020-12-04 22:07:28 -0300

    guessinggame.sh seems to be running completely. Might revise later

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,6 +1,32 @@
 echo "Welcome to The Guessing Game"
 echo " "
+echo "Take a guess about how many files are in this directory"
 
-echo "Enter something just for fun: "
-read response
-echo "You entered : $response"
+file_number=$(ls -l | egrep -v "drw|total" | wc -l)
+echo $file_number
+
+function get_guess {
+	echo "Enter your guess: "
+	read response
+}
+
+get_guess
+
+while [[ response -ne file_number ]]
+do
+	if [[ response -lt file_number ]]
+	then
+		echo "Your guess was too low"
+	elif [[ response -gt file_number ]]
+	then
+		echo "Your guess was to high"
+	fi
+	
+	get_guess
+done
+
+if [[ response -eq file_number ]]
+then
+	echo "Congratulations! Your guess was right!"
+	echo "You won The Guessing Game"
+fi
